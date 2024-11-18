d1cdf8b1fc6668903ae2dee39cbb658ee050d4b5
Juca Venancio <juca.svenancio@gmail.com>
2024-11-18 17:48:14 -0300

    Testing the logic

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-//app.js
-alert("Teste");
+alert("Welcome to our website");
+
+
+name = prompt("What's your name?");
+idade = prompt("");
+numberOfSales = 50;
+balanceAvailable = 1000.00;
+
+errorMessage = "Error! Fill in all fields!!!";
+
+if(idade >= 18)
+{
+    alert("If you are of legal age, you can get your license.");
+}
+alert(errorMessage);
+
